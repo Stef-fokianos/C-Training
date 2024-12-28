8067ef681820ff63e6d3ccee96705ddf46e4a3fd
ST3F-LENOVO\St3f <stfokianos@gmail.com>
2024-12-28 12:13:03 +0200

    Solution for task 1.12

---
+++ Task1.12/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Sockets;
+
+namespace Task1._12
+{
+    public class Menu
+    {
+        private string[] options = { "1. Pizza", "2. Burger", "0. Exit" };
+
+        public void DisplayMenu()
+        //Diplay Menu options
+        {
+            Console.WriteLine("Please pick a choice: ");
+            foreach (string option in options)
+            {
+                Console.WriteLine(option);
+            }
+
+        }
+
+        public void Selection(string userInput)
+        //Output based on choice
+        {
+            switch(userInput)
+            {
+                case "1":
+                    Console.WriteLine("Your selection is Pizza.");
+                    break;
+                case "2":
+                    Console.WriteLine("Your selection is Burger.");
+                    break;
+                case "0":
+                    Console.WriteLine("Exiting application.");
+                    break;
+                default:
+                    Console.WriteLine("Wrong selection!");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Task1.12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.Design;
+
+// Δημιουργήστε ένα Console Application το οποίο θα δείχνει στον χρήστη ένα μενού με τουλάχιστον 2 επιλογές 
+// φαγητού και μία επιλογή για έξοδο από το πρόγραμμα, η οποία θα έχει τιμή “0” (μηδέν) και θα είναι τελευταία στη λίστα.
+//  Όταν ο χρήστης επιλέγει κάποια από τις επιλογές, το Application πρέπει να δείχνει στην κονσόλα ποιο φαγητό επέλεξε 
+//  και μετά απλά να τερματίζει.
+// Υποχρεωτικά specs:
+// Οι επιλογές που θα δείχνει το Application, πρέπει να είναι αποθηκευμένες σε ένα Array.
+// Οι επιλογές πρέπει να προβάλλονται στην κονσόλα με κάποιο loop.
+// Σε περίπτωση που ο χρήστης δεν επιλέξει κάποια επιλογή από το μενού, αλλά ούτε και “Έξοδο” από το application, το application πρέπει να δείξει το μήνυμα “Wrong selection!” και να τερματίζει.
+
+namespace Task1._12
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Menu initiation and display 
+            Menu menu = new Menu();
+            menu.DisplayMenu();
+
+            //Handle user input
+            Console.WriteLine("Please type your selection: ");
+            string userInput = Console.ReadLine();
+            menu.Selection(userInput);
+
+        }
+    }
+}
