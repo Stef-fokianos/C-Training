63db16d17afe3ec38b2995daf8ddbb98110bab0c
ST3F-LENOVO\St3f <stfokianos@gmail.com>
2025-01-03 10:49:23 +0200

    Solution for task1.16

---
+++ Task1.16/Menu.cs
@@ -0,0 +1,79 @@
+﻿using System.Net.Sockets;
+
+namespace Task1._16
+{
+    public class Menu
+    {   
+        //Choices initiation 
+        private IFood exit = new Exit();
+        private IFood pizza = new Pizza();
+        private IFood burger = new Burger();
+        private IFood[] options;
+
+        public Menu()
+        { 
+            //Menu options to include 
+            options = new IFood[] {pizza, burger, exit};
+        }
+
+
+        public void GetMenuItems()
+        //Display Menu options
+        {
+            Console.WriteLine("Menu:");
+            foreach (IFood option in options)
+            {
+                Console.WriteLine($"{option.OptionNumber} : {option.FoodTitle}");
+            }
+
+        }
+
+        public void Selection(int userInput)
+        //Output based on choice
+        {
+            int intUserInput = Convert.ToInt32(userInput);
+
+            bool found = false;
+            //Display selected choice
+            foreach (IFood option in options)
+            {
+                if (intUserInput == option.OptionNumber)
+                {
+                    Console.WriteLine($"You selected: {option.FoodTitle}");
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                Console.WriteLine("Please enter a valid menu choice.");
+            }
+           
+        }
+
+        public interface IFood 
+        {
+            string FoodTitle {  get; set; }
+            int OptionNumber { get; set; }
+        }
+
+        public class Exit : IFood
+        {
+            public int OptionNumber { get; set; } = 0;
+            public string FoodTitle { get; set; } = "Exit";
+        }
+
+        public class Pizza : IFood
+        {
+            public int OptionNumber { get; set; } = 1;
+            public string FoodTitle { get; set; } = "Pizza";
+        }
+
+        public class Burger : IFood
+        {
+            public int OptionNumber { get; set; } = 2;
+            public string FoodTitle { get; set; } = "Burger";
+        }
+
+       
+    }
+}
---
+++ Task1.16/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.Design;
+using System.Xml;
+
+// Δημιουργήστε ένα interface με όνομα “IFood” το οποίο θα περιλαμβάνει ένα string property με όνομα “FoodTitle” και ένα int property
+//  με όνομα “OptionNumber”. 
+// Δημιουργήστε μία κλάση για το κάθε φαγητό του μενού, η οποία θα υλοποιεί το συγκεκριμένο interface.
+// Αλλάξτε το array των επιλογών ώστε να περιέχει όλα αυτά τα αντικείμενα φαγητού, που υλοποιούν το interface “IFood”.
+// Το array με τις επιλογές φαγητού πρέπει να έρχεται από άλλη κλάση, από μέθοδο που να λέγεται “GetMenuItems”.
+// Αντικαταστήστε τις κλήσεις τις Int32.TryParse με Convert.ToInt32. Δεν πρέπει να υπάρχει πουθενά κλήση της Int32.TryParse 
+// Φροντίστε, με το κατάλληλο error handling, η εφαρμογή να λειτουργεί κανονικά. Σε περίπτωση exception, να δείχνει ένα κατάλληλο 
+// μήνυμα στον χρήστη και να μην έχει crash.  
+// Η εφαρμογή πρέπει να μην τερματίζει, εκτός και αν ο χρήστης το επιλέξει. 
+
+namespace Task1._16
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Menu initiation
+            Menu menu = new Menu();
+
+            //Responsible for stoping the execution  
+            bool continueProgram = true;
+
+            do
+            {
+                //Print menu
+                menu.GetMenuItems();
+
+                //Handle user input
+                Console.WriteLine("Please type your selection: ");
+                string userInput = Console.ReadLine();
+                
+                try
+                {   
+                    //Convert input and display choice
+                    int intUserInput = Convert.ToInt32(userInput);
+                    menu.Selection(intUserInput);
+
+                    if (intUserInput == 0)
+                    {
+                        //Exit was selected by user
+                        Console.WriteLine("Thank you, goodbye!");
+                        continueProgram = false;
+                    }
+                }catch (Exception ex)
+                {   
+                    //Invalid input
+                    Console.WriteLine($"An unexpected error occured: {ex.Message}");   
+                }
+
+            } while (continueProgram);
+            
+        }
+    }
+}
